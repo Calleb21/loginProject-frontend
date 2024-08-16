6a5f68c2babf736120176954e104a776194e25b8
Calleb Camargo <camargocalleb12@gmail.com>
2024-08-15 22:35:47 -0300

    update-15-08-2024

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Router } from '@angular/router';
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(appRef => {
+    const router = appRef.injector.get(Router);
+    router.events.subscribe(() => {
+      const body = document.body;
+      if (router.url === '/login') {
+        body.classList.add('no-scroll');
+      } else {
+        body.classList.remove('no-scroll');
+      }
+    });
+  })
   .catch((err) => console.error(err));
